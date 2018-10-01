280594d1d59fbe18091a51ad4eec97a1552f8424
20151074010697 <20151074010697@MC266461.ifrn.local>
2018-10-01 11:03:19 -0300

    criou a classe AppWebConfiguration, habilitou webMVC do string e configurou o ComponentScan.

--- agendaspring/src/agendaspring/config/ServeletSpringMVC.java
+++ agendaspring/src/agendaspring/config/ServeletSpringMVC.java
@@ -13,7 +13,7 @@ public class ServeletSpringMVC extends AbstractAnnotationConfigDispatcherServlet
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Class[] {AppWebConfiguration.class};
 	}
 
 	@Override
